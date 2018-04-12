7c0aa35bbf0f5c14bebdc47cbf46b96e24fa881b
Sutirtha Marjit <sutirtha.marjit@ericsson.com>
2018-04-12 17:35:33 +0530

    file create

--- util/application.js
+++ util/application.js
@@ -1,23 +1,51 @@
 module.exports = (express,app,port,bodyParser,MasterService)=>{    
 
     const service = new MasterService();
+    let urlencodedParser = bodyParser.urlencoded({ extended: false });
     app.use(express.static('front-end'));
     app.use('/admin',express.static('admin'));
     app.use('/app',express.static('application-sites'));
-    app.get('/services/get/',(req,res)=>{
+    app.get('/services/get/:id',(req,res)=>{
         res.type('json');
-        res.json({status:"ok"});
+        res.json({status:"ok",body:req.params.id});
     });
-
-    app.post('/services/post/',(req,res)=>{
-        res.type('json');
-        res.json({status:"post"});
-    });
-
     
-
+    //app.post("")
+    
+    
+    
     app.listen(port,()=>{
         console.log('Server is running at '+port);
     });
     
 };
+
+
+/*
+
+app.post('/login', urlencodedParser, function (req, res) {
+        if (!req.body) return res.sendStatus(400)
+        res.send( req.body)
+      })
+    
+    app.post('/services/post/fileupload',urlencodedParser,(req,res)=>{
+        
+    });
+    
+    app.post('/services/post/',urlencodedParser,(req,res)=>{
+        res.type('json');
+        console.log(req.body);
+        if(req.body){
+            res.json({
+                response:req.body.x,
+                error:'Value'
+            })
+        }else{
+            res.json({
+                response:"null",
+                error:'no data'
+            })
+        }
+    });
+
+*/
--- util/service.js
+++ util/service.js
@@ -1,8 +1,33 @@
+const fs = require('fs');
 class MasterService{
 
     constructor(){
-        this.serviceEngineInitTime = new Date();
+      this.serviceEngineInitTime = new Date();
     }
+
+    getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    getFolderName(){
+
+        const alpha = String.fromCharCode(65+this.getRandomInt(1,26));
+        
+        return `${alpha}A${parseInt(Math.random()*1000*this.getRandomInt(10,90))}${(new Date()).getTime()}`;
+    }
+
+    createFolder(){        
+        var folderName = `application-sites/${this.getFolderName()}`  
+        if(!fs.existsSync(folderName)){
+            console.log(folderName);
+            try{
+                fs.mkdirSync(folderName);
+            }catch(e){
+                console.log(e); 
+            }
+            
+        }
+    }    
 
 }
 
