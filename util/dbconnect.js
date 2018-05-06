e90dd9ef2b6021fa52f185789e790682ea40e5d9
Sutirtha Lenovo Laptop <sutirtha.social.marjit@gmail.com>
2018-05-06 20:43:32 +0530

    Mlab save

--- util/dbconnect.js
+++ util/dbconnect.js
@@ -2,16 +2,19 @@ const mongoose = require('mongoose');
 const userObject = require("./dbschema/user");
 
 const Schema = mongoose.Schema;
-const User = mongoose.model("User",mongoose.Schema(userObject));
+const User = mongoose.model("User",mongoose.Schema(userObject,{collection:'appuserbase'}));
 
 class DBConnect{
 
     constructor(){
+      /*
       const username = "SUtirtha-Marjit";
-      const password = "punisepaku#@1234";
+      const password = "punisepaku#@1234";*/
+      const username = "admin";
+      const password = "admin";
       const dburiWithPassValue = `mongodb://${username}:${password}@ds163689.mlab.com:63689/dbwebapprunner`;
       const dburi = `mongodb://${username}@ds163689.mlab.com:63689/dbwebapprunner`;
-      /*
+      
       let cr_user = new User({
             userid:"xdtopa09091",
             persona:{
@@ -22,25 +25,36 @@ class DBConnect{
                 username:'sutirtha.marjit@gmail.com',
                 passcode:'LXT45@123'
             }
-      });*/
+      });
       
       //console.dir(cr_user);
-      
-      //this.conn = mongoose.createConnection(dburi);
       /*
-      mongoose.connect(dburi).then(()=>{
-          console.log(`DB Connected to ${dburi}`);
-          cr_user.save().then(()=>{
-              console.log('Saved successfully');
+      this.conn = mongoose.createConnection(dburi);
+      if(this.conn){
+          console.log(`Great! Connection established with ${dburi}`);
+      }*/
+      
+      var saveUser = ()=>{
+        cr_user.save().then(()=>{
+            console.log('Saved successfully');
+        }).catch((error)=>{
+            console.log('Error occured while saving data MLab');
+            console.dir(error);
+        });
+      }
+
+      mongoose.connect(dburiWithPassValue).then(()=>{
+          console.log(`Great! Connection established with ${dburiWithPassValue}`);
+          saveUser();
+          
+          }).catch((error)=>{
+          console.log("Error occured while saving the data");
           })
-      }).catch((error)=>{
-          console.log(error);
-      })
-     */
+     
       
       
     }
 
-}
+}  
 
 module.exports = DBConnect;
